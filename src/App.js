89a3ee9dace3ab473b1c47918009bbbf90799ec9
dev-brit <52476511+dev-brit@users.noreply.github.com>
2022-09-21 21:23:20 -0500

    create header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
+import Header from "./components/Header"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header/>
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+
+  )
 }
 
-export default App;
+Header.defaultProps = {
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95',
+}
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+}
+
+export default App
