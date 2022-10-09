cc3421a9d9605b6decf1a478890934dfb4ee2fe3
dev-brit <52476511+dev-brit@users.noreply.github.com>
2022-10-09 17:29:28 -0500

    Create editFeedback function

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -24,21 +24,37 @@ export const FeedbackProvider = ({children}) => {
     },
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
+  // add feedback item
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback]) 
   }
 
+  // delete feedback item
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
+  // set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit:true
+    })
+  }
+
   return <FeedbackContext.Provider value={{
     feedback,
     deleteFeedback,
-    addFeedback
+    addFeedback,
+    editFeedback
   }}>
     {children}
   </FeedbackContext.Provider>
